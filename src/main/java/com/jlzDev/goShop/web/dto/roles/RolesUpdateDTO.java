570f407046732778a752d443dccb5602f81b888b
jose <joseluis836@hotmail.com>
2025-05-10 09:21:01 -0500

    Add roles controller

--- src/main/java/com/jlzDev/goShop/web/dto/roles/RolesUpdateDTO.java
+++ src/main/java/com/jlzDev/goShop/web/dto/roles/RolesUpdateDTO.java
@@ -1,5 +1,8 @@
 package com.jlzDev.goShop.web.dto.roles;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +13,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RolesUpdateDTO {
+    @NotNull
     private Integer idRol;
+    @NotBlank
     private String nombre;
+    @Size(max = 255)
     private String descripcion;
 }
--- src/main/java/com/jlzDev/goShop/web/mapper/RolesDTOMapper.java
+++ src/main/java/com/jlzDev/goShop/web/mapper/RolesDTOMapper.java
@@ -1,8 +1,11 @@
 package com.jlzDev.goShop.web.mapper;
 
 import com.jlzDev.goShop.domain.model.Roles;
+import com.jlzDev.goShop.web.dto.roles.RolesCreateDTO;
 import com.jlzDev.goShop.web.dto.roles.RolesDTO;
+import com.jlzDev.goShop.web.dto.roles.RolesUpdateDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -17,4 +20,16 @@ public interface RolesDTOMapper {
 
     // DTO to Domain
     Roles toRol(RolesDTO rolDTO);
+
+    // CreateDTO to Domain
+    @Mapping(target = "idRol", ignore = true) // Ignoramos el ID ya que será generado
+    @Mapping(target = "createdAt", ignore = true) // Ignoramos la fecha de creación
+    @Mapping(target = "updatedAt", ignore = true) // Ignoramos la fecha de actualización
+    Roles toRolFromCreateDTO(RolesCreateDTO createRolDTO);
+
+    // Nuevo método para el DTO de actualización
+    @Mapping(target = "usuarios", ignore = true) // Asumiendo que tienes esta relación
+    @Mapping(target = "createdAt", ignore = true) // Se establecerá manualmente
+    @Mapping(target = "updatedAt", ignore = true) // Se establecerá manualmente
+    Roles toRolFromUpdateDTO(RolesUpdateDTO updateDTO);
 }
